cb3e9d90ba4f018b9cb495f83650044b9b9bba97
Luis Ochoa <eduardo.leo8a@gmail.com>
2018-09-28 23:00:16 -0500

    Token en header

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -1,9 +1,10 @@
 const jwt = require('jsonwebtoken');
 module.exports = (req,res,next)=>{
     try{
-    const decoded = jwt.verify(req.body.token,"secret");
-    req.userData = decoded;
-    next();
+        const token = req.headers.authorization.split(" ")[1];
+        const decoded = jwt.verify(token,"secret");
+        req.userData = decoded;
+        next();
     }catch(error){
         return res.status(401).json({
             message: 'Auth failed'
